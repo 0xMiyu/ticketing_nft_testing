f6ed359311ae4355e3fd5c6f793b6eaac7ce43b1
AstraI <ong.martin24@gmail.com>
2023-02-06 15:55:52 +0800

    cleanup codebase

--- components/CreateCollection.tsx
+++ components/CreateCollection.tsx
@@ -1,8 +1,5 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import {
-    Transaction,
-    PublicKey,
-} from "@solana/web3.js";
+import { Transaction, PublicKey, Connection } from "@solana/web3.js";
 import { FC, useCallback } from "react";
 import { NFTStorage, Blob, File } from "nft.storage";
 import {
@@ -10,10 +7,11 @@ import {
     walletAdapterIdentity,
     TransactionBuilder,
 } from "@metaplex-foundation/js";
+import { nftStorage } from "@metaplex-foundation/js-plugin-nft-storage";
 
-//test
 export const CreateCollection: FC = () => {
-    const { connection } = useConnection();
+    // const { connection } = useConnection();
+    const connection = new Connection(process.env.RPC_ENDPOINT!);
     const { publicKey, sendTransaction } = useWallet();
     const wallet = useWallet();
 
@@ -50,16 +48,10 @@ export const CreateCollection: FC = () => {
             alert("Wallet not Connected!");
             return;
         }
-        const metaplex = Metaplex.make(connection).use(
-            walletAdapterIdentity(wallet)
-        );
-        const nftstorage_client = new NFTStorage({
-            token: NFTSTORAGE_API_KEY!,
-        });
+        const metaplex = Metaplex.make(connection)
+            .use(walletAdapterIdentity(wallet))
+            .use(nftStorage({ token: NFTSTORAGE_API_KEY! }));
 
-        // const img_data : any = readFileSync("/Users/martin/Stuff/Projects/journalist_next_testing/ticket_img.png");
-        // const type : any = mime.getType("/Users/martin/Stuff/Projects/journalist_next_testing/ticket_img.png");
-        // const nft_img = new File(img_data, "ticketimg", type)
         const temp_nft_img_link =
             "https://nftstorage.link/ipfs/bafybeiflvon2jde7ww4rftt2wzdjqn2bek7ixeixbm6aib52fbluqqerem/1042.png";
         const temp_nft_img_ipfs =
@@ -89,16 +81,13 @@ export const CreateCollection: FC = () => {
                     creators: ticketData.royalties,
                 },
             };
-            const blob = new Blob([JSON.stringify(metadata, null, 2)], {
-                type: "application/json",
-              });
-            const cid = await nftstorage_client.storeBlob(blob);
-            console.log("====================")
-            console.log(cid);
-            const temp_metadata_uri =
-                "https://bafkreicagv2rcxcgyn67ptn7iycyysm2tl4s4wnnrrb4j76lyztygqh2ey.ipfs.nftstorage.link/";
-
             
+            // const temp_metadata_uri =
+            //     "https://bafkreicagv2rcxcgyn67ptn7iycyysm2tl4s4wnnrrb4j76lyztygqh2ey.ipfs.nftstorage.link/";
+            const uri = await metaplex.nfts().uploadMetadata(metadata);
+            console.log("===Metadata URI===");
+            console.log(uri);
+
             alert("Transaction Confirmed!");
         } catch (error: any) {
             alert(error);
@@ -107,10 +96,14 @@ export const CreateCollection: FC = () => {
     }, [publicKey, connection]);
 
     return (
-        <div style = {{height: "60px"}}>
+        <div style={{ height: "60px" }}>
             <button
                 onClick={onClick}
-                style = {{height: "60px", background: 'slateblue', borderRadius: "10%"}}
+                style={{
+                    height: "60px",
+                    background: "slateblue",
+                    borderRadius: "10%",
+                }}
             >
                 <span>Create Collection NFT</span>
             </button>
